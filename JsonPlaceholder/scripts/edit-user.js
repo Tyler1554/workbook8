5606d4421df39956a3a4c09c553bd981b53f21e4
Tyler1554 <tyler1554@yahoo.com>
2023-12-13 15:25:29 -0500

    project complete

--- JsonPlaceholder/scripts/edit-user.js
+++ JsonPlaceholder/scripts/edit-user.js
@@ -4,22 +4,43 @@ const emailField = document.querySelector("#email-field");
 const submitEditUser = document.querySelector("#submit-edited-user");
 const urlParams = new URLSearchParams(location.search);
 
-function fetchUser() {
-  let id = 0;
-  if (urlParams.has("id") === true) {
-    id = urlParams.get("id");
-    fetch(`http://localhost:3000/users/${id}`)
-      .then((response) => response.json())
-      .then((user) => {
-        inputUserData(user);
-      });
+
+  function loadUser() {
+    let id = -1;
+    if (urlParams.has("id") === true) {
+      id = urlParams.get("id");
+      fetch(`http://localhost:3000/users/${id}`)
+        .then((response) => response.json())
+        .then((user) => {
+          nameField.value = user.name;
+          userNameField.value = user.username;
+          emailField.value = user.email;
+        });
+    }
   }
-}
 
-function inputUserData(user) {
-  nameField.innerText = user.name;
-  userNameField.innerText = user.username;
-  emailField.innerText = user.email;
-}
-
-window.init = inputUserData;
+  function editUser() {
+    let editedName = nameField.value;
+    let editedUserName = userNameField.value;
+    let editedEmail = emailField.value;
+    const editeduser = {
+      name: `${editedName}`,
+      username: `${editedUserName}`,
+      email: `${editedEmail}`
+    };
+    let id = urlParams.get("id");
+    fetch(`http://localhost:3000/users/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editeduser),
+    })
+    .then((responese) => responese.json())
+    .then((user) => {
+      window.location.href = "users.html"
+    });
+    }
+    
+    window.onload = loadUser;
+    submitEditUser.onclick = editUser;
